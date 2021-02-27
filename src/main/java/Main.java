2f49315fc8aba3c25caffe59e89eb4fecb7949f5
Danil <pushind00@yandex.ru>
2021-02-27 11:51:28 +0300

    removed excessive message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I'm a new java app!");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = scanner.next();
