519668c6f4fa2fa4732424925f235db92900aea4
Danil <pushind00@yandex.ru>
2021-02-27 11:19:24 +0300

    new message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Integer a = 1 / 0; // severe bug;
+        System.out.println("I'm a new java app!");
     }
 }
