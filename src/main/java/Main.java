ac05e60420d7b27b0931743801e99da3b8dcc6ba
Danil <pushind00@yandex.ru>
2021-02-27 11:53:03 +0300

    removed more excessive messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,6 @@ public class Main {
         System.out.println("Enter your name: ");
         String name = scanner.next();
         System.out.printf("Hello, %s!%n", name);
-        System.out.println("¯\\_(ツ)_/¯");
         System.out.println("Enter borders of guess number game:");
         long left = scanner.nextLong();
         long right = scanner.nextLong();
