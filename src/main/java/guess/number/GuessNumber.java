2304ac5ea4dcf768946f0221d4e038d9c428c992
Danil <pushind00@yandex.ru>
2021-02-27 11:55:06 +0300

    fixed case sensitivity

--- src/main/java/guess/number/GuessNumber.java
+++ src/main/java/guess/number/GuessNumber.java
@@ -1,5 +1,6 @@
 package guess.number;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class GuessNumber {
@@ -9,7 +10,7 @@ public class GuessNumber {
             long middle = (right_border + left_border) / 2;
             System.out.println("Is your number less than " + middle + "?");
             String ans = scanner.next();
-            if (ans.equals("yes")) {
+            if (ans.toLowerCase(Locale.ROOT).equals("yes")) {
                 right_border = middle;
             } else {
                 left_border = middle;
