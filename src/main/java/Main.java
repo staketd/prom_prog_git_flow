5111de1c04f4c597e57339c6b55082060965e61d
Danil <pushind00@yandex.ru>
2021-02-27 11:23:37 +0300

    Greetings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Integer a = 1 / 0; // severe bug;
         System.out.println("I'm a new java app!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write your name: ");
+        String name = scanner.next();
+        System.out.printf("Hello, %s!%n", name);
     }
 }
